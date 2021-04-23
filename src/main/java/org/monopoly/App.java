e93439f536ed5185342d8cbb9d3d5635b4139d24
js414 <js414@hdm-stuttgart.de>
2021-04-23 20:47:50 +0200

    init

---
+++ src/main/java/org/monopoly/App.java
@@ -0,0 +1,11 @@
+package org.monopoly;
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi there,\n let's have\n         fun learning Java!");
+    }
+}
