f0b930298e349d9e04a555a34dba5845a85f3c9a
Marcel <mw225@hdm-stuttgart.de>
2021-04-27 18:02:19 +0200

    Konto und Banke erstellt

---
+++ src/main/java/org/monopoly/Bank.java
@@ -0,0 +1,28 @@
+package org.monopoly;
+
+public class Bank {
+
+    public Bank() {
+
+    }
+
+    private final int ueberLos = 200;
+    private final int genauAufLos = 400;
+    private final int startkapital = 3000;
+
+    public int getUeberLos() {
+        return ueberLos;
+    }
+
+    public int getGenauAufLos() {
+        return genauAufLos;
+    }
+
+    public int getStartkapital() {
+        return startkapital;
+    }
+
+
+
+
+}
---
+++ src/main/java/org/monopoly/Spieler.java
@@ -0,0 +1,36 @@
+package org.monopoly;
+
+public class Spieler {
+    Bank bank;
+    Konto konto;
+
+    public Spieler() {
+        this.bank = new Bank();
+        this.konto = new Konto(bank.getStartkapital());
+    }
+
+    //Nach jeder Änderung des Kontostandes wird der neue Kontostand über das return statement zurückgegeben.
+    //Falls wir später eine Anzeige des Kontostandes haben, kann man die Anzeige mit den Returnstatments aktualisieren
+
+    public int spielerUeberLos(){
+        konto.geldGutschreiben(bank.getUeberLos());
+        return konto.getKontostand();
+    }
+    public int spielerGenauAuflos(){
+        konto.geldGutschreiben(bank.getGenauAufLos());
+        return konto.getKontostand();
+    }
+    public int spielerKauftGrundstück(){
+        konto.geldAbziehen(100); //Die 100 ist nur ein Beispielbetrag hier wird später die Variable des Grundstückpreises Übergeben
+        return konto.getKontostand();
+    }
+
+    public int spielerZahltMiete(){
+        konto.geldAbziehen(10); //Die 10 ist nur ein Beispielbetrag hier wird später die Variable der zu zahlenden Miete übergeben
+        return konto.getKontostand();
+    }
+    public int spielerErhaeltMiete(){
+        konto.geldGutschreiben(10); //Die 10 ist nur ein Beispielbetrag hier wird später die Variable der zu erhaltenden Miete übergeben
+        return konto.getKontostand();
+    }
+}
