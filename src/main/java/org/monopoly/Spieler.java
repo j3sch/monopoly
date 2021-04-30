ed72ebe001e2d0f9129e5ab02896f85e6dca2b20
ts204 <ts204@hdm-stuttgart.de>
2021-04-30 11:44:14 +0200

    Name und Farbe

--- src/main/java/org/monopoly/Spieler.java
+++ src/main/java/org/monopoly/Spieler.java
@@ -3,10 +3,30 @@ package org.monopoly;
 public class Spieler {
     Bank bank;
     Konto konto;
+    String name;
+    String color;
 
-    public Spieler() {
+    public Spieler(String name, String color) {
+        this.name = name;
+        this.color = color;
         this.bank = new Bank();
         this.konto = new Konto(bank.getStartkapital());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     //Nach jeder Änderung des Kontostandes wird der neue Kontostand über das return statement zurückgegeben.
