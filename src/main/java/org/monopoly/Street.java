3b2cc327bc0b59eebe305a9fe818a5d252ee5f4a
FabiMacBook <fs135@hdm-stuttgart.de>
2021-05-04 14:56:12 +0200

    implementation street and streetmanager

---
+++ src/main/java/org/monopoly/Street.java
@@ -0,0 +1,50 @@
+package org.monopoly;
+
+public class Street {
+//Eigenschaften
+//Properties
+
+    private String streetName;
+    private boolean sold;
+    private int price;
+    private int rent;
+    private String color;
+
+//Konstruktor
+//Constructor
+    public Street(String streetName, boolean sold, int price, int rent, String color) {
+        this.streetName = streetName;
+        this.sold = sold;
+        this.price = price;
+        this.rent = rent;
+        this.color = color;
+    }
+//Methods
+//Getter
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public boolean isSold() {
+        return sold;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+//Setter
+
+
+    public void setSold(boolean sold) {
+        this.sold = sold;
+    }
+}
