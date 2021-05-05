6ead9a4859a6664ac226c0c0365c4415d9fe07af
Marcel <mw225@hdm-stuttgart.de>
2021-05-05 12:31:27 +0200

    Klassen zu neuem Projekt verschoben

---
+++ src/main/java/com/hdm/monopoly/Bank.java
@@ -0,0 +1,27 @@
+package com.hdm.monopoly;
+
+public class Bank {
+
+    public Bank() {
+
+    }
+
+    private final int ueberLos = 200;
+    private final int genauAufLos = 400;
+    private final int startkapital = 3000;
+
+    public int getUeberLos() {
+        return ueberLos;
+    }
+
+    public int getGenauAufLos() {
+        return genauAufLos;
+    }
+
+    public int getStartkapital() {
+        return startkapital;
+    }
+
+
+
+}
--- src/main/java/com/hdm/monopoly/MonopolyApplication.java
+++ src/main/java/com/hdm/monopoly/MonopolyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonopolyApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(MonopolyApplication.class, args);
 	}
 
