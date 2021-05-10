f5f078d2b281fff16f8a257e84922c2c606c6978
marcelwagner <mw225@hdm-stuttgart.de>
2021-05-10 11:19:40 +0200

    Klasse 'Bank' Geändert

--- src/main/java/com/hdm/monopoly/backened/player_money/Spieler.java
+++ src/main/java/com/hdm/monopoly/backened/player_money/Spieler.java
@@ -5,36 +5,25 @@ import com.hdm.monopoly.backened.player_money.Konto;
 
 public class Spieler {
 
-    Bank bank;
-    Konto konto;
+
+    private Konto konto;
 
     public Spieler() {
-        this.bank = new Bank();
-        this.konto = new Konto(bank.getStartkapital());
+
+        this.konto = new Konto(3000);
     }
 
     //Nach jeder Änderung des Kontostandes wird der neue Kontostand über das return statement zurückgegeben.
     //Falls wir später eine Anzeige des Kontostandes haben, kann man die Anzeige mit den Returnstatments aktualisieren
 
-    public int spielerUeberLos(){
-        konto.geldGutschreiben(bank.getUeberLos());
-        return konto.getKontostand();
-    }
-    public int spielerGenauAuflos(){
-        konto.geldGutschreiben(bank.getGenauAufLos());
-        return konto.getKontostand();
-    }
-    public int spielerKauftGrundstück(){
-        konto.geldAbziehen(100); //Die 100 ist nur ein Beispielbetrag hier wird später die Variable des Grundstückpreises Übergeben
+
+    public int spielerErhaeltGeld(int betrag){
+        konto.geldGutschreiben(betrag);
         return konto.getKontostand();
     }
 
-    public int spielerZahltMiete(){
-        konto.geldAbziehen(10); //Die 10 ist nur ein Beispielbetrag hier wird später die Variable der zu zahlenden Miete übergeben
-        return konto.getKontostand();
-    }
-    public int spielerErhaeltMiete(){
-        konto.geldGutschreiben(10); //Die 10 ist nur ein Beispielbetrag hier wird später die Variable der zu erhaltenden Miete übergeben
+    public int spielerZahltGeld(int betrag){
+        konto.geldAbziehen(betrag);
         return konto.getKontostand();
     }
 }
