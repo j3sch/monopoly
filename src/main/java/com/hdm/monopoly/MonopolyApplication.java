6e7e2206e21942d73e934069f8cdb70b2242b07f
Schlegel Jens <js414@hdm-stuttgart.de>
2021-05-21 10:25:38 +0000

    Update MonopolyApplication.java

--- src/main/java/com/hdm/monopoly/MonopolyApplication.java
+++ src/main/java/com/hdm/monopoly/MonopolyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MonopolyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MonopolyApplication.class, args);
 	}
-
 }
