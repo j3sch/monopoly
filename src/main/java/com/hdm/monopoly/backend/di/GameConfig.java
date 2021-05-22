0ebaef2d928e03e66b2e66e2fb7c6d22c695a25c
js414 <js414@hdm-stuttgart.de>
2021-05-23 00:35:31 +0200

    removed sendMessage in GameConfig

--- src/main/java/com/hdm/monopoly/backend/di/GameConfig.java
+++ src/main/java/com/hdm/monopoly/backend/di/GameConfig.java
@@ -2,7 +2,6 @@ package com.hdm.monopoly.backend.di;
 
 import com.hdm.monopoly.backend.board.streets.StreetManager;
 import com.hdm.monopoly.backend.player_money.Player;
-import com.hdm.monopoly.backend.player_money.SendMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +13,6 @@ public class GameConfig {
     private final Player[] players = new Player[4];
     private final StreetManager streetManager = new StreetManager();
     private final String[] sessionIds = new String[4];
-    private final SendMessage sendMessage = new SendMessage();
 
     @Bean
     public Player[] getPlayers() {
