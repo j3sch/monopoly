709ef9e92a109ea6e0734d713db5f86f5b728e07
js414 <js414@hdm-stuttgart.de>
2021-05-22 18:02:28 +0200

    array size sold from 15 to 16

--- src/main/java/com/hdm/monopoly/backened/board/streets/StreetManager.java
+++ src/main/java/com/hdm/monopoly/backened/board/streets/StreetManager.java
@@ -9,7 +9,7 @@ public class StreetManager {
 //properties
 
 final private String[]  streetName = {"Go", "Burger Joint", "Pizza House", "Chance", "Candy Store","Ice Cream Palor", "Jail", "Museum", "Library", "Chance", "Skate Park", "Swimming Pool", "FreeParking", "Video Game Arcade", "Movie Theater", "Chance", "Toy Store", "Pet Store", "GoToJail", "Bowling Alley", "The Zoo", "Chance", "Park Place", "Boardwalk"};
-private boolean[] sold = new boolean[15];
+private boolean[] sold = new boolean[16];
 final private int[] price = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,5,5};
 final private int[] rent = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,5,5};
 final private String[]  color = {"Brown", "Brown", "Light Blue","Light Blue", "Pink", "Pink", "Orange","Orange","Red","Red", "Yellow", "Yellow","Green", "Green" , "Dark Blue", "Dark Blue"};
@@ -23,10 +23,9 @@ private int subtractor = 0;
 
 //Methods
 
-
 //For schleife erstellt die Objekte und fügt sie der ArrayListe hinzu.
     public void streetMaker() {
-        for(int i = 0; i < 23; i++) {
+        for(int i = 0; i < 24; i++) {
             if (i % 3 == 0) {
                 Street street = new Street(this.streetName[i]);
                 this.streets.add(street);
