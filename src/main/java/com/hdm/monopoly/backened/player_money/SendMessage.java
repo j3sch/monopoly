dc0ab94b2a185374ede45bd68db219cca43429ab
js414 <js414@hdm-stuttgart.de>
2021-05-23 00:15:55 +0200

    sendMessage

---
+++ src/main/java/com/hdm/monopoly/backened/player_money/SendMessage.java
@@ -0,0 +1,36 @@
+package com.hdm.monopoly.backened.player_money;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Component("sendMessage")
+public class SendMessage {
+
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public void setMessagingTemplate(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    protected void sendToUser(String sessionId, String destination, String message) {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor
+                .create(SimpMessageType.MESSAGE);
+        headerAccessor.setSessionId(sessionId);
+        headerAccessor.setLeaveMutable(true);
+
+        messagingTemplate.convertAndSendToUser(sessionId, destination,
+                message,
+                headerAccessor.getMessageHeaders());
+    }
+
+    protected void sendToAll(String destination, String message) {
+        messagingTemplate.convertAndSend(destination,
+                message);
+    }
+}
